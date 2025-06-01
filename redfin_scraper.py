6bb3d7006e11d464eab675fd7caa09273d746b1c
sgonuguntla <34722297+sgonuguntla@users.noreply.github.com>
2025-06-01 19:48:41 -0400

    Update redfin_scraper.py

--- redfin_scraper.py
+++ redfin_scraper.py
@@ -28,3 +28,10 @@ def get_redfin_sales(zip_code, months=24, min_sales=3):
         return avg_price, top_sales
     else:
         return None, []
+
+if __name__ == "__main__":
+    avg_price, comps = get_redfin_sales("20148")
+    print("Average Price:", avg_price)
+    print("Comparables:")
+    for c in comps:
+        print(c)
